9157527f4b6a37f9f144d2323cb518065292a381
Alber701H <alber701hamuyela@gmail.com>
2026-02-01 21:19:47 +0000

    admin dashboar / calendar fixed

---
+++ cur10us/src/components/ui/ThemeToggle.tsx
@@ -0,0 +1,18 @@
+// "use client"
+// import { Sun, Moon } from "lucide-react"
+// import { useTheme } from "@/provider/theme"
+
+// const ThemeToggle = () => {
+//   const { theme, toggleTheme } = useTheme()
+
+//   return (
+//     <button
+//       onClick={toggleTheme}
+//       className="p-2 rounded-full bg-gray-100 dark:bg-zinc-800 hover:bg-gray-200 dark:hover:bg-zinc-700 transition-colors"
+//     >
+//       {theme === "light" ? <Moon size={20} /> : <Sun size={20} />}
+//     </button>
+//   )
+// }
+
+// export default ThemeToggle
